e4906006a2c9c3889fe9e2d4c628c8765d90e4a0
Sawixoo <Sawixoo@users.noreply.github.com>
2021-09-21 09:51:02 +0600

    21.09.2021

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+# 13.09.21
 from work_1.linear import *
 from work_1.forking import *
 from work_1.Loop_with_precondition import *
 from work_1.Loop_with_postcondition import *
 from work_1.Additional_tasks import *
+# 21.09.21
+from work_2.work_2 import *
+from work_2.conditional_operators import *
 
 
 if __name__ == '__main__':
-    a = float(input('Введите А: '))
-    b = float(input('Введите В: '))
-    c = float(input('Введите C: '))
-    # x = float(input('Введите X: '))
-    # y = float(input('Введите Y: '))
-    print(first_task(a, b))       # Ввести название нужной функции
+    # a = float(input('Введите А: '))
+    # b = float(input('Введите В: '))
+    # c = float(input('Введите C: '))
+    # # x = float(input('Введите X: '))
+    # # y = float(input('Введите Y: '))
+    task_if_5()       # Ввести название нужной функции
